92f2fbbea2c4a8c76abe7e670d3d5ac04aeebb35
Xin lIU <xliu0@cs.washington.edu>
2019-11-26 10:35:34 -0800

    specify GPU

--- train.py
+++ train.py
@@ -276,6 +276,8 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     hparams = create_hparams(args.hparams)
+    os.environ["CUDA_VISIBLE_DEVICES"]="1"
+
 
     torch.backends.cudnn.enabled = hparams.cudnn_enabled
     torch.backends.cudnn.benchmark = hparams.cudnn_benchmark
